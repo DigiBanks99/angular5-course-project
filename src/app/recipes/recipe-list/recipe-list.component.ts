87860e9741e0c36dae717adc595fe364a31abf1c
Wilco Boshoff <wlboshoff@gmail.com>
2018-02-13 06:34:10 +0200

    Bind recipe detail section to list click events.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -9,12 +9,22 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
-    new Recipe('My Recipe', 'We will putt some info here.', 'https://cdn.pixabay.com/photo/2017/07/16/10/43/recipe-2508859_960_720.jpg')
+    new Recipe('Mixed pepper crusted steak with mushroom salad',
+    'Keep summer cooking easy with this clever pepper-crusted steak and salad meal.',
+    'http://img.taste.com.au/-OoAuH3l/w643-h428-cfill-q90/taste/2016/11/mixed-pepper-crusted-steak-with-mushroom-salad-87602-1.jpeg'),
+    new Recipe('Pasta a la Italian',
+    'Some tasty Italian Pasta to make your mouth water.',
+    'https://cdn.pixabay.com/photo/2017/11/11/10/07/pasta-2938710_960_720.jpg')
   ];
+
+  @Output() recipeSelectedChanged = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRecipeSelectedChanged(recipe: Recipe) {
+    this.recipeSelectedChanged.emit(recipe);
+  }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import {Recipe} from './recipe.model';
 
 @Component({
   selector: 'app-recipes',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent {
+  @Input() selectedRecipe: Recipe;
 
+  constructor() { }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.selectedRecipe = recipe;
+  }
 }
